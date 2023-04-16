ce8127877d30dd7735249aed9c4ef157801f28aa
Ahmed Soliman <58696140+AhmedSoliman00@users.noreply.github.com>
2023-04-16 22:19:12 +0200

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,74 @@
+
+public class Account {
+
+    private String name = null;
+    private int id = 0;
+    private double balance = 0;
+    private double annualInterestRate = 0;
+    private double counter;
+
+    Account() {
+        this.name = "Ahmed";
+        this.id = 100;
+        this.balance = 500;
+        this.annualInterestRate = 1;
+        counter = 20020;
+    }
+
+    Account(String newName, int id_no, int newbalance) {
+        this.name = newName;
+        this.id = id_no;
+        this.balance = newbalance;
+        counter++;
+    }
+
+    public double withDraw(double amountOfWithdraw) {
+        if (balance > amountOfWithdraw) {
+            return this.balance - amountOfWithdraw;
+        } else {
+            System.out.println("your balance is smaller than" + amountOfWithdraw + "\n exit");
+        }
+        return balance;
+    }
+
+    public double deposit(double amountOfDepoisit) {
+        this.balance = this.balance + amountOfDepoisit;
+        return this.balance;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setAnnualInterestRate(double annualInterestRate) {
+        this.annualInterestRate = annualInterestRate;
+    }
+
+    public double getAnnualInterestRate() {
+        return annualInterestRate;
+    }
+
+
+    public double getMonthlyInterRate() {
+        return this.getMonthlyInterRate() / 12;
+    }
+
+    public double getMonthlyInter() {
+        return this.getMonthlyInterRate() - 50.0 / 12;
+    }
+}
+
+
+
